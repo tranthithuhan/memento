19a8e679bf24689745052d36cdb416a9a8c1d8e9
Han <tranthithuhan@gmail.com>
2021-11-24 22:56:21 +0100

    versioning track

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("hello")
+import Caretaker from './Caretaker'
+import Track from './Track'
+
+var caretaker = new Caretaker();
+var track = new Track({ name: "song 1" });
+
+caretaker.save(track);
+
+track.setName("update 1");
+caretaker.save(track);
+
+track.setName("update 2");
+caretaker.save(track);
+
+track.setName("update 3");
+caretaker.save(track);
+
+track.setName("update 4");
+caretaker.save(track);
+
+track.setName("update 5");
+caretaker.save(track);
+
+console.log("4", caretaker.getHistory());
