31e463d7ab229ccb3d96233e61e0afd44541e7db
Han <tranthithuhan@gmail.com>
2021-11-24 22:14:30 +0100

    init track class

---
+++ src/Track.ts
@@ -0,0 +1,26 @@
+class Track {
+	name: string
+	creationDate: number
+	
+	constructor({ name = null, creationDate = null }) {
+		this.name = name
+		this.creationDate = creationDate
+	}
+	
+	setName(name: string) {
+		this.name = name
+	}
+	
+	setCreationDate(creationDate: number) {
+		this.creationDate = creationDate
+	}
+	
+	getTrack() {
+		return {
+			name: this.name,
+			creationDate: this.creationDate
+		}
+	}
+}
+
+export default Track
